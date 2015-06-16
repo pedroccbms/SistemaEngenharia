6191fba2cb1e5b8fb81b5f479f475073719bfa18
Anderson <Anderson@Asus>
2015-06-15 22:25:08 -0300

    abcd

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -17,6 +17,8 @@ public class Teste {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
+         System.out.println("teste");
+        
     }
     
 }
